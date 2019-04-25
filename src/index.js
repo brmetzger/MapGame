d8f9dbf43046684a538bce5e8b33bc85a4cb7c23
gitrog <37446909+gitrous@users.noreply.github.com>
2019-04-25 14:14:52 -0400

    Display random locations on the client

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 //Require external modules
-const fs = require("fs");
-const http = require("http");
-    const port = 80;
-const ip = require("ip");
+const FS = require("fs");
+const HTTP = require("http");
+    const PORT = 80;
+const IP = require("ip");
 
-fs.readFile('./web/index.html', (err, html) => {
+//Require internal modules
+const LOCATIONS = require("./data/locations.json");
+
+FS.readFile('./web/index.html', (err, html) => {
     if (err) {
         //Failed to load file
         throw err; 
     };
     //Create the server
-    let server = http.createServer((request,response) => {
-        response.writeHeader(200, {"Content-Type":"text/html"});
-        response.end(html.toString().replace("${IPADDRESS}",ip.address()));
+    let server = HTTP.createServer((request,response) => {
+        if (request.url == "/locations.json") {
+            //Get the list of locations
+            response.writeHeader(200);
+            response.end(JSON.stringify(LOCATIONS));
+        } else {
+            //Loaded the web page normally
+            response.writeHeader(200, {"Content-Type":"text/html"});
+            response.end(html.toString().replace("${IPADDRESS}",IP.address()));
+        };
     });
     
     //Listen to the server
-    server.listen(port);
-    console.log(`Map game running on port ${port}`);
+    server.listen(PORT);
+    console.log(`Map game running on port ${PORT}`);
 });
